888971afe32caacb46c35a5a53524dc11e205709
vlad727 <41288192+vlad727@users.noreply.github.com>
2024-04-24 13:33:54 +0300

    Update main.go
    added os.WriteFile and os.ReadDir
    also added time.Sleep to keep pod is running

--- main.go
+++ main.go
@@ -4,15 +4,38 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
 
-	// read file
+	// read and copy file kube-apiserver.yaml to kube-apiserver.yaml.bac
 	audit, err := os.ReadFile("/config/kube-apiserver.yaml")
 	if err != nil {
 		log.Println(err)
 	}
+	log.Println("Success read the file...")
 
-	fmt.Println(string(audit))
+	// Write data to dstination file
+	err = os.WriteFile("/config/kube-apiserver.yaml.bac", audit, 0600)
+	if err != nil {
+		log.Printf("Can't create copy for file kube-apiserver.yaml\n")
+		log.Println(err)
+	}
+
+	// list files
+	files, err := os.ReadDir("/config/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name(), file.IsDir())
+	}
+	log.Println("Success list the files...")
+
+	//go to a sleep
+	time.Sleep(3600 * time.Second)
 }
+
+
